c5eb2f3eb3589bdff0bf1a494507ba95dfd239c6
Pirax161 <axeliteriteka6@gmail.com>
2026-02-26 15:54:12 +0000

    Fix: Initialize app before exporting v2

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-if (require.main === module) { 
- app.listen(3000, () => console.log("API running on port 3000")); 
-} 
-module.exports = app;
-
 const tasksRouter = require('./routes/tasks'); 
 
 // CI test change 
@@ -15,6 +10,11 @@ console.log("Testing CI with a Pull Request");
 app.get('/', (req, res) => { 
  res.json({ message: "Task Manager API running (Lab3" }); 
 });
+
+if (require.main === module) { 
+ app.listen(3000, () => console.log("API running on port 3000")); 
+} 
+module.exports = app;
 
 app.use('/tasks', tasksRouter);
 

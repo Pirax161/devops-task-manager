778557e8bb27673a2800e5e610d6177684e595a2
Pirax161 <axeliteriteka6@gmail.com>
2026-02-26 15:50:30 +0000

    Fix: Initialize app before exporting

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
 if (require.main === module) { 
  app.listen(3000, () => console.log("API running on port 3000")); 
 } 
 module.exports = app;
-
-const express = require('express');
-const app = express();
-app.use(express.json());
 
 const tasksRouter = require('./routes/tasks'); 
 

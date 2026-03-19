0e4921634cf3e238141fa02ebed6ed255ee2c836
Pirax161 <axeliteriteka6@gmail.com>
2026-03-19 20:39:19 +0000

    Add Docker build step

--- src/app.js
+++ src/app.js
@@ -4,18 +4,22 @@ app.use(express.json());
 
 const tasksRouter = require('./routes/tasks'); 
 
-// CI test change 
-console.log("Testing CI with a Pull Request");
-
-app.get('/', (req, res) => { 
- res.json({ message: "Task Manager API running (Lab3" }); 
-});
-
-if (require.main === module) { 
- app.listen(3000, () => console.log("API running on port 3000")); 
-} 
-module.exports = app;
-
+// Routes
 app.use('/tasks', tasksRouter);
 
-app.listen(3000, ()=> console.log("API running on port 3000"));
+app.get('/', (req, res) => { 
+    res.json({ message: "Task Manager API running (Lab 4)" }); 
+});
+
+// GESTION DU PORT (Crucial pour Docker)
+const PORT = process.env.PORT || 3000;
+
+// On ne lance le serveur QUE si ce fichier est exécuté directement
+// Cela évite les conflits avec les tests et les doublons
+if (require.main === module) { 
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`API running on port ${PORT}`);
+    });
+} 
+
+module.exports = app;
